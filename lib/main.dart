f05ac380a0b061ecf6535385c41262a623c7564d
Yoran Huzen <info@yoranhuzen.nl>
2020-02-20 13:40:46 +0100

    Changes

--- lib/main.dart
+++ lib/main.dart
@@ -18,9 +18,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.red,
+        primarySwatch: Colors.green,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Arriva Afname'),
     );
   }
 }
@@ -105,7 +105,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.mail_outline),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
