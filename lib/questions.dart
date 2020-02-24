6f82653ee65291d0bbee534601f3eb63f0684869
Yoran Huzen <info@yoranhuzen.nl>
2020-02-24 09:47:18 +0100

    Added subtitle

--- lib/questions.dart
+++ lib/questions.dart
@@ -22,7 +22,9 @@ class QuestionListState extends State<QuestionList> {
                   canTapOnHeader: true,
                   headerBuilder: (BuildContext context, bool isExpanded) {
                     return ListTile(
+                      contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 0),
                       title: item.headerValue,
+                      subtitle: item.headerSub,
                     );
                   },
                   body: ListTile(
@@ -36,25 +38,27 @@ class QuestionListState extends State<QuestionList> {
 }
 
 class QuestionBox {
-  QuestionBox(String headerText, String expandedText) {
+  QuestionBox(String headerText, String headerSub, String expandedText) {
     this.headerValue = Text(headerText);
+    this.headerSub = Text(headerSub);
     this.expandedValue =
         Container(child: Text(expandedText), padding: EdgeInsets.all(10));
     this.isExpanded = false;
   }
 
-  Widget expandedValue;
   Text headerValue;
+  Text headerSub;
+  Widget expandedValue;
   bool isExpanded;
 }
 
 final List<QuestionBox> questions = [
-  QuestionBox("Title 1",
+  QuestionBox("Title 1", "Subtitle 1",
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam porttitor ligula in eleifend mattis. In hac habitasse platea dictumst. Donec ullamcorper purus tempor sem iaculis ullamcorper nec at magna. Curabitur tempor diam blandit massa laoreet auctor. Aenean a sem blandit, mattis ex vel, sagittis eros. Nulla a magna laoreet velit pretium tincidunt. Nullam id varius quam. Pellentesque semper dui lorem, in malesuada diam venenatis molestie."),
-  QuestionBox("Header 2",
+  QuestionBox("Header 2", "Subtitle 2",
       "Nulla facilisi. Nam sed erat vel lorem suscipit finibus. Aliquam tincidunt vulputate justo, sed faucibus nisi gravida vel. Donec in ultricies risus. Nam ut nunc mauris. Nullam viverra maximus leo, vitae elementum turpis tristique eu. Nam velit arcu, consequat in risus eu, varius tincidunt enim. Phasellus tempus augue ut sapien tincidunt, ac venenatis leo maximus. Maecenas maximus nec ante et sagittis. Duis ac condimentum magna, nec lobortis ipsum. Sed rutrum aliquam leo vitae luctus. Aliquam eget semper turpis, eget dapibus dolor. Sed pellentesque mi id commodo vehicula. Nulla eu rhoncus sem, at tempus justo. Vestibulum massa odio, fringilla vitae aliquam non, sollicitudin vel turpis."),
-  QuestionBox("Header 3",
+  QuestionBox("Header 3", "Subtitle 3",
       "Fusce interdum est sapien, eget molestie libero convallis non. Etiam imperdiet facilisis condimentum. Aenean pretium augue tortor, eget commodo quam dictum non. Aenean dapibus orci non dui ornare ornare. Praesent pulvinar ut mauris quis gravida. Nullam mattis viverra justo, eget posuere velit imperdiet sed. Donec bibendum id nulla eu rutrum. Duis feugiat, neque ut ultrices pellentesque, lectus dui mollis purus, et aliquet erat velit eget enim. Nulla nunc enim, convallis et dolor non, faucibus maximus erat. Curabitur sed pharetra libero. Proin at metus ligula. Integer eget lorem tortor. Pellentesque sodales felis turpis, vel imperdiet nibh commodo at."),
-  QuestionBox("Foo",
+  QuestionBox("Foo", "Bar makes a return",
       "Mauris at enim consectetur, ultricies lorem sit amet, porttitor lectus. Nam cursus consectetur egestas. Nunc aliquet urna sit amet lacus porta facilisis vitae ac velit. Phasellus sollicitudin, lacus non posuere imperdiet, elit leo auctor ligula, in aliquet massa nisi dictum lectus. Vivamus auctor dapibus commodo. Curabitur in accumsan orci. Vivamus turpis risus, accumsan vel porta a, fermentum ac quam. Pellentesque posuere nisi vitae libero maximus tempor. Nunc consectetur ipsum tellus, et ultricies sapien dignissim non. Aenean ornare ex sed ornare venenatis. Duis blandit nisl nec arcu blandit efficitur. Praesent sit amet neque vitae sem cursus malesuada. Cras eget pretium ipsum."),
 ];
